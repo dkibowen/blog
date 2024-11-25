ed36a4b0a29031e874b345a8f355498abff63a38
Douglas Kibowen <dowenkim@gmail.com>
2024-11-25 18:03:51 +0300

    added blogs models and sample blogs

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ("title","category","author","status","is_featured")
+    list_editable = ("status","is_featured")
+    search_fields = ("title","category__category_name","status")
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
 
 
