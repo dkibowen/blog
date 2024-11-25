ab6b3343b5a2e636323ec96b5c2e4c1d80b56d16
Douglas Kibowen <dowenkim@gmail.com>
2024-11-25 23:58:23 +0300

    displaying categories and blog posts in the home page

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from blogs.models import Category,Blog
 
 
 def home(request):
-    return render(request,'home.html')
+    categories = Category.objects.all().order_by("-created_at")
+    featured_posts = Blog.objects.filter(is_featured=True,status="Published").order_by("-created_at")
+    posts = Blog.objects.filter(is_featured=False,status="Published")
+    context = {
+        "categories":categories,
+        "featured_posts":featured_posts,
+        "posts":posts,
+    }
+    return render(request,'home.html',context)
